e68b416a00ecaa3760d78a76d2382ead8d74ca08
Dan Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2021-06-20 15:20:24 -0500

    Tweak text

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ function Layout({ children }) {
       <main className="flex-1 w-full max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-16">
         {children}
       </main>
-      <Footer note={<a href="https://github.com/SimmonsRitchie/gatsby-v3-tailwind-starter">View on Github</a>} />
+      <Footer note={<a href="https://github.com/SimmonsRitchie/gatsby-v3-tailwind-starter">View on GitHub</a>} />
     </div>
   );
 }
